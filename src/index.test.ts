3c6b32df3901694b44d33a7d2d0a13bee2c23127
Carolina Chico <jcchicom@unal.edu.co>
2023-06-26 21:12:24 -0500

    V0.1.2

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,28 +2,32 @@
 import request from 'supertest'
 import App from './Router'
 
-describe (
-    //Caracteristicas de nombre de la prueba
-    'GET/',
-    ()=>{
-        let app:App
+describe(
+	'GET /',
+	()=>{
+        
+		let app:App        
+        
         beforeAll(
             ()=>{
                 app=new App()
                 app.start()
             }
         )
-        afterAll(// se ejecuta despues de la prueba
-            app.close()
+
+        afterAll(
+            ()=>{
+                app.close()
+            }           
         )
-        //declarar la prueba
+
         test(
             'Debe devolver un mensaje',
             async ()=>{
-                const res =await request(app.app).get('/')
+                const res = await request(app.app).get('/')
                 expect (res.statusCode).toEqual(200)
-                expect(res.text).toEqual('Bienvenido a Typescript')
+                expect (res.text).toEqual('Bienvenidos a typescript')
             }
         )
-    }
+	}
 )
